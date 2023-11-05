5c74874a4f05b2e5d16f614a7b435cd046966c62
Larionov Alexandr <a.f.larionov@gmail.com>
2023-11-05 23:38:54 +0300

    - fix version

--- src/main/java/m3/lib/repositories/UserStuffRepository.java
+++ src/main/java/m3/lib/repositories/UserStuffRepository.java
@@ -1,7 +1,14 @@
 package m3.lib.repositories;
 
 import m3.lib.entities.UsersStuffEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserStuffRepository extends CrudRepository<UsersStuffEntity, Long> {
+
+    @Modifying
+    @Query(value = "INSERT INTO users_points(userId, pointId, score) VALUES (?1, ?2, ?3)" +
+            "ON DUPLICATE KEY UPDATE score = ?3", nativeQuery = true)
+    void updateUsersPoints(Long userId, Long pointId, Long score);
 }
