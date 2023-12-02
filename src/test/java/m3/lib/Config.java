eae1168c17b3dfd4a964b32dccaa52731b39a5e8
Larionov Alexandr <a.f.larionov@gmail.com>
2023-12-02 20:14:57 +0300

    - fix log tests - move repo test to m3-lib - add gradle to root m3 project

---
+++ src/test/java/m3/lib/Config.java
@@ -0,0 +1,9 @@
+package m3.lib;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class Config {
+}
---
+++ src/test/java/m3/lib/repositories/UserAgentRepositoryTest.java
@@ -0,0 +1,47 @@
+package m3.lib.repositories;
+
+import m3.lib.BaseDataJpaTest;
+import m3.lib.entities.UserAgentEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Commit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest(properties = {
+        "logging.level.org.springframework.jdbc.core=DEBUG",
+        "logging.level.org.hibernate.SQL=DEBUG"
+})
+@Commit
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserAgentRepositoryTest extends BaseDataJpaTest {
+
+    @Autowired
+    private UserAgentRepository userAgentRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void testSaveOne() {
+        // given
+        final var uid = 123L;
+        final var agent = "user-agent-string2";
+
+        jdbcTemplate.update("DELETE FROM `user_agents`");
+        final var entity = UserAgentEntity.builder()
+                .uid(uid)
+                .agent(agent)
+                .build();
+        // when
+        userAgentRepository.save(entity);
+
+        // then
+        final var data = jdbcTemplate.queryForMap("SELECT * FROM `user_agents`");
+        assertEquals(uid, Long.valueOf((Integer) data.get("uid")));
+        assertEquals(agent, data.get("agent"));
+    }
+}
