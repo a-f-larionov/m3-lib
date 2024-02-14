42178457ad25604234ec6c2bebe435d925cda49c
a.f.larionov <a.f.larionov@gmail.com>
2024-02-14 14:13:30 +0300

    - add some test

---
+++ src/test/java/m3/lib/ApplicationTest.java
@@ -0,0 +1,11 @@
+package m3.lib;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest extends BaseSpringBootTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
--- src/test/java/m3/lib/BaseSpringBootTest.java
+++ src/test/java/m3/lib/BaseSpringBootTest.java
@@ -1,16 +1,17 @@
 package m3.lib;
 
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
-public class BaseDataJpaTest {
+public class BaseSpringBootTest {
+
     static final MySQLContainer<?> mySQLContainer;
 
     static {
@@ -23,8 +24,17 @@ public class BaseDataJpaTest {
         mySQLContainer.start();
     }
 
+    static final KafkaContainer kafkaContainer;
+
+    static {
+        kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))
+                .withReuse(true);
+        kafkaContainer.start();
+    }
+
     @DynamicPropertySource
     private static void DynamicPropertySource(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
     }
 }
